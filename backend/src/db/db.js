4992abe191c1d6e2ce3ca0b79485e80f16677735
a7mdRadwan <ahmed.radwan@in3.cloud>
2020-12-15 21:58:34 +0200

    add nodemin and mongoose libs declare fb const in angular app

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,20 +3,20 @@ const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
 const database = 'Auth&Auz';      // REPLACE WITH YOUR DB NAME
 
 class Database {
- options = {useNewUrlParser: true ,useUnifiedTopology: true ,useFindAndModify:false}
+  options = { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
   constructor() {
-    this._connect()
+    this._connect();
   }
-_connect() {
-     mongoose.connect(`mongodb://${server}/${database}`,this.options)
-       .then(() => {
-         console.log('Database connection successful :) ♥ ')
-       })
-       .catch(err => {
-         console.log('Database connection error :(')
-       })
+  _connect() {
+    mongoose.connect(`mongodb://${server}/${database}`, this.options)
+      .then(() => {
+        console.log('Database connection successful :) ♥ ');
+      })
+      .catch(err => {
+        console.log('Database connection error :(');
+      });
   }
 }
 
 
-module.exports = new Database()
+module.exports = new Database();
--- web-client/src/app/components/log-in/log-in.component.ts
+++ web-client/src/app/components/log-in/log-in.component.ts
@@ -1,39 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import {  FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { AlertService } from 'src/app/services/alert.service';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+
+declare const FB: any;
+
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
-    loading = false;
-    submitted = false;
-    returnUrl: string;
-    
-    loginForm = new FormGroup({
-       email : new FormControl(null , [Validators.required , Validators.email]),
-       password : new FormControl(null ,  [Validators.required])
-     
-   })
-    
-   constructor(
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+
+  loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required])
+
+  })
+
+  constructor(
     private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService,
     private _alertService: AlertService) {
-          // get return url from route parameters or default to '/'
-          this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    }
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
 
   ngOnInit(): void {
   }
   //  convenience getter for easy access to form fields
   get f() {
-    return this.loginForm.controls; 
+    return this.loginForm.controls;
   }
   onSubmit() {
     this.submitted = true;
@@ -41,40 +44,40 @@ export class LogInComponent implements OnInit {
     this._alertService.clear();
     // stop here if form is invalid
     if (this.loginForm.invalid) {
-        return;
+      return;
     }
     this.loading = true;
     this.authenticationService.login(this.f.email.value, this.f.password.value)
-        .pipe(first())
-        .subscribe(
+      .pipe(first())
+      .subscribe(
         // successs
-            data => {
-            console.log("Data From Log in" , data)
-                this.router.navigate([this.returnUrl]);
-            },
-            // error
-            error => {
+        data => {
+          console.log("Data From Log in", data)
+          this.router.navigate([this.returnUrl]);
+        },
+        // error
+        error => {
           //  console.log("Error" , error)
-                this._alertService.error(error);
-                this.loading = false;
-            });
+          this._alertService.error(error);
+          this.loading = false;
+        });
   }
-  // user cabpility to login with facebook 
-  onLogFb(){
-   FB.getLoginStatus((response)=>{
-      if(response.status ==="connected"){
-      console.log(response)
-         this.router.navigate(["./home"])
-      } 
-      else{
-        FB.login((loginResponse)=>{
+  // user cabpility to login with facebook
+  onLogFb() {
+    FB.getLoginStatus((response) => {
+      if (response.status === "connected") {
+        console.log(response)
+        this.router.navigate(["./home"])
+      }
+      else {
+        FB.login((loginResponse) => {
           this.router.navigate(["./home"])
         })
       }
-   
-   })
-  
-  
+
+    })
+
+
   }
 
 }
