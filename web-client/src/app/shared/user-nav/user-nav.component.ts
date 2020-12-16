e91980052e03c517e93fff33e51c7c84e20217d5
MHMDAZAB <MhmdGezo1912@gmail.com>
2020-12-17 01:02:52 +0200

    enable user search members Articles && starting create aservice to fetch data of user articles

--- web-client/src/app/shared/user-nav/user-nav.component.ts
+++ web-client/src/app/shared/user-nav/user-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-nav',
@@ -7,10 +9,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./user-nav.component.scss']
 })
 export class UserNavComponent implements OnInit {
+  private articleSubject = new Subject<string>();
 userProfile 
+
+// private _articleService: ArticleService
+
   constructor(private route:ActivatedRoute) {
 
-    this.route.params.subscribe((params)=>{
+  this.route.params.subscribe((params)=>{
      console.log(params)
      this.userProfile = params
 
@@ -18,6 +24,18 @@ userProfile
    }
 
   ngOnInit(): void {
+    const articlePosts$ = this.articleSubject.pipe(
+      debounceTime(550),
+      distinctUntilChanged(),
+      map(userId =>console.log(userId))
+      // switchMap(userId => this.blogService.fetchPosts(userId))
+    );
   }
 
+ 
+  searchArticles(searchTerm:string){
+    console.log("User Search Term  " ,searchTerm)
+    this.articleSubject.next(searchTerm)
+  }
+ 
 }
